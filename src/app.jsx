daeb220aa667f223a7a96429383d2a9511705aef
Viorel Cojocaru <vio@beanon.com>
2023-10-11 23:37:18 +0200

    Initial commit

---
+++ src/app.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from 'antd';
+
+import { HomePage } from './pages/home';
+import { RepoDetailsPage } from './pages/repo-details';
+import { AboutPage } from './pages/about';
+import { NotFoundPage } from './pages/not-found';
+
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import css from './app.module.css';
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Header />
+        <div className={css.contentContainer}>
+          <Layout.Content className={css.content}>
+            <Routes>
+              <Route path="/repos/:owner/:repo" element={<RepoDetailsPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Layout.Content>
+        </div>
+        <Footer />
+      </Layout>
+    </BrowserRouter>
+  );
+}
